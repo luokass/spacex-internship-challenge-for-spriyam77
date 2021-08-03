23b267a87e11cec4c9ef45e9f383c1be9f1887d7
luokass7 <luokass07@gmail.com>
2021-08-03 20:51:35 +0530

    deployed

---
+++ spacexdashboard/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAPgzrpohnwf6ZoP-BBIruQGbA73seS0FE",
+    authDomain: "spacexdash-769ee.firebaseapp.com",
+    projectId: "spacexdash-769ee",
+    storageBucket: "spacexdash-769ee.appspot.com",
+    messagingSenderId: "480223685949",
+    appId: "1:480223685949:web:81e710af22203832002954",
+    measurementId: "G-KKVRTK8BF6"
+  };
